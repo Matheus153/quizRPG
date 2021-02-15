4d7ed4321e9983d56f9df3b860699850d61f95a3
mathe <santiosmatheus@gmail.com>
2021-02-15 16:14:04 -0400

    master

--- pages/index.js
+++ pages/index.js
@@ -80,17 +80,7 @@ export default function Home() {
           </Widget>
         </StyledContainer>
         
-      <GitHubCorner
-      as={motion.div}
-      transition={{ delay: 0.2, duration: 0.55 }}
-      variants={{
-        show: { opacity: 1, y: '0' },
-        hidden: { opacity: 0, y: '100%'}
-      }}
-      initial="hidden"
-      animate="show"
-      
-      projectUrl="https://github.com/Matheus153"></GitHubCorner>
+      <GitHubCorner projectUrl="https://github.com/Matheus153"/>
     </QuizBackground>
     
   )
--- pages/quiz/index.js
+++ pages/quiz/index.js
@@ -4,9 +4,6 @@ import { ThemeProvider } from 'styled-components';
 import QuizScreen from '../../src/screens/Quiz';
 import db from '../../db.json';
 
-import GitHubCorner from '../../src/components/GitHubCorner'
-import { motion } from 'framer-motion'
-
 export default function QuizDaGaleraPage() {
   return (
     <ThemeProvider theme={db.theme}>
@@ -14,17 +11,6 @@ export default function QuizDaGaleraPage() {
         externalQuestions={db.questions}
         externalBg={db.bg1}
       />
-      <GitHubCorner
-      as={motion.div}
-      transition={{ delay: 0.2, duration: 0.55 }}
-      variants={{
-        show: { opacity: 1, y: '0' },
-        hidden: { opacity: 0, y: '100%'}
-      }}
-      initial="hidden"
-      animate="show"
-      
-      projectUrl="https://github.com/Matheus153"></GitHubCorner>
     </ThemeProvider>
   );
 }
